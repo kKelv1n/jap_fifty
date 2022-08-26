77f9faec09618c38221ab717963cd04aaff07f08
kKelv1n <kevin.yhd0@gmail.com>
2022-08-26 08:34:40 +0800

    update: ask random type question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from jap_fifty.question import *
+from jap_fifty import utils
 
 
 def print_result(is_right: bool, right_ans: str):
@@ -46,13 +47,10 @@ def ask_h_by_k(q: QuestionItf):
 
 
 if __name__ == '__main__':
+    qs = [ask_h_by_r, ask_k_by_r, ask_k_by_h, ask_r_by_h, ask_r_by_k, ask_h_by_k]
     while True:
         q = Selection()
-        ask_h_by_r(q)
-        ask_k_by_r(q)
-        ask_k_by_h(q)
-        ask_r_by_h(q)
-        ask_r_by_k(q)
-        ask_h_by_k(q)
+        idx = utils.rand_n(len(qs))
+        qs[idx](q)
 
 
